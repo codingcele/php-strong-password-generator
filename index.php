92a077d093ff72d24bcbe2afd6f495a636cd4cc5
codingcele <marcocelegato9@gmail.com>
2023-01-12 20:00:15 +0100

    get

--- index.php
+++ index.php
@@ -4,9 +4,30 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Strong password generator</title>
 </head>
+
+<style>
+    *{
+        font-family: Verdana;
+    }
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+</style>
+
 <body>
-    
+    <form action="">
+        <label for="psw_length">Lunghezza password:</label>
+        <input type="number" min="1" name="length">
+        <input type="submit" value="GENERATE">
+    </form>
+
+    <?php
+        $length = $_GET['length'] ?? false;
+    ?>
+
 </body>
 </html>
